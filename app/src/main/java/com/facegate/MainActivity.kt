3474a03f309ab96bfe2b15281af0891edb1639b2
Yash Krishan Gupta <yashkrishangupta@gmail.com>
2026-06-18 16:27:55 +0530

    Add button click listeners for student and admin

--- app/src/main/java/com/facegate/MainActivity.kt
+++ app/src/main/java/com/facegate/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.facegate
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.facegate.ui.admin.AdminDashboard
+import com.facegate.ui.attendance.AttendanceFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
             insets
+        }
+
+        val btnStudent = findViewById<Button>(R.id.btnStudent)
+        val btnAdmin = findViewById<Button>(R.id.btnAdmin)
+
+        btnStudent.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, com.facegate.ui.attendance.AttendanceFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+
+        btnAdmin.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, com.facegate.ui.admin.AdminDashboard())
+                .addToBackStack(null)
+                .commit()
         }
     }
 }
