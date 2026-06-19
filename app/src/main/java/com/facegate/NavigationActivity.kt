2ec06b774e79e21e6a9a9ee82e03ab2585aa3682
Yash Krishan Gupta <yashkrishangupta@gmail.com>
2026-06-19 15:51:45 +0530

    hilt update

--- app/src/main/java/com/facegate/NavigationActivity.kt
+++ app/src/main/java/com/facegate/NavigationActivity.kt
@@ -2,7 +2,9 @@ package com.facegate
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NavigationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
